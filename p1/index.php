8600dfd24ba09b66df3aefa9644dcc9cce59dd7b
cristhian-sanjines <115581330+cristhian-sanjines@users.noreply.github.com>
2022-10-11 14:27:35 -0400

    Add files via upload

---
+++ p1/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Paguina web</title>
+
+	<link rel="stylesheet" href="style.php">
+</head>
+<body>
+	<header>
+		<section id="logoUmsa">
+			<a href="index.php"><img src="imaguen/umsa.logo.png"></a>
+		</section>
+		<section id="tituloPrincipal">
+			<h3>Universidad Mayor de San Andres</h3>
+			<h5>Facultad de Ciencias Puras y Naturales</h5>
+		</section>
+	</header>
+	<hr>
+	<p id="bienvenida">Bienvenidos a la facultad de Ciencias Puras y Naturales</p>
+	<table border="1px">
+		<thead>
+			<tr>
+				<th>CARRERA</th>
+				<th>ESTUDIANTES</th>
+				<th>ACCESO</th>
+			</tr>
+		</thead>
+		<tr>
+			<td>INFORMATICA</td>
+			<td>5</td>
+			<td><a href="./inf/principal.php">Acceso</a></td>
+		</tr>
+		<tr>
+			<td>MATEMATICA</td>
+			<td>3</td>
+			<td><a href="./mat/principal.php">Acceso</a></td>
+		</tr>
+		<tr>
+			<td>FISICA</td>
+			<td>3</td>
+			<td><a href="./fis/principal.php">Acceso</a></td>
+		</tr>
+	</table>
+</body>
+</html>
